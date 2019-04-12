6fad22a1b1051a928f493488e6243524ce34c619
unknown <nathaliecortz@hotmail.com>
2019-04-11 22:12:36 -0500

    Tests and functions first issue; pathTrue, fileExist, fileValidationMd and isDir

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,38 @@
-const mdLinks = require('../');
+const path = require("../lib/path.js");
 
 
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('pathTrue',()=>{
+  it('should be "Function"',()=>{
+    expect(typeof path.pathTrue).toBe("function");
   });
+  it('Should be "false', ()=>{
+    expect(path.pathTrue()).toBe(false);
+  });
+  it('Should be "true', ()=>{
+    expect(path.pathTrue("../README.md")).toBe(true);
+  });
+});
 
+describe('fileExist', ()=>{
+  it ('Should be "true"',()=>{
+    expect(path.fs.exists("../README.md")).toBe(true);
+  });
+});
+
+describe('fileValidationMd',()=>{
+  it ('Should be "true"', ()=>{
+    expect(path.fileValidationMd("../README.md")).toBe(true);
+});
+  it ('Should be "false"', ()=>{
+    expect(path.fileValidationMd("../README.txt")).toBe(false);
+});
+});
+
+describe('isDir',()=>{
+  it ('Should be "true"', ()=>{
+    expect(path.isDir("C:/Users/Usuario Principal/Laboratoria/DataLoversFinal")).toBe(true);
+});
+  it ('Should be "false"', ()=>{
+    expect(path.isDir("../README.txt")).toBe(false);
+});
 });
