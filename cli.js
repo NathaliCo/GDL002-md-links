092ba3365bdfdf15e8e5fe77a3199d9e074a7704
unknown <nathaliecortz@hotmail.com>
2019-05-05 22:34:30 -0500

    Actualización de package.json

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const mdLinks=require('.lib/index');
+const mdLinks=require('./lib/index.js');
 let pathFile = process.argv[2];
 let options = process.argv[3];
 mdLinks.mdLinks(pathFile, options);
