65eb5595e0b123cd7c8a53393d7c665517ffc691
unknown <nathaliecortz@hotmail.com>
2019-05-05 22:21:48 -0500

    Correción de ruta en index en cli

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const mdLinks=require('.lib/index.js');
+const mdLinks=require('.lib/index');
 let pathFile = process.argv[2];
 let options = process.argv[3];
 mdLinks.mdLinks(pathFile, options);
