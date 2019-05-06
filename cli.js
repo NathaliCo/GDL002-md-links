9c10813dfb0ba9295caaea9e9f8c7a61ce56faa5
unknown <nathaliecortz@hotmail.com>
2019-05-05 21:24:24 -0500

    Modificación de cli.js

--- cli.js
+++ cli.js
@@ -1,4 +1,4 @@
 const mdLinks=require('.lib/index.js');
 let pathFile = process.argv[2];
 let options = process.argv[3];
-mdLinks.mdLinks.mdLinks(pathFile, options);
+mdLinks.mdLinks(pathFile, options);
